5278d09e2c6ed4f18f7fc1589a2713aa11322603
Jared Pavan <thatsjared87@gmail.com>
2014-06-15 11:11:27 -0400

    Updated ids

--- cah/sql_scripts/deal_card.sql
+++ cah/sql_scripts/deal_card.sql
@@ -2,10 +2,10 @@
 -- TAKES INPUT user_id, room_id, card_type (black/white)
 
 INSERT INTO `hands` (room_id, user_id, card_id, card_status)
-SELECT @room_id, @user_id, card_id, 'dealt'
+SELECT @room_id, @user_id, id, 'dealt'
 FROM `cards`
 WHERE card_type = @card_type
-	AND card_id NOT IN
+	AND id NOT IN
 	(
 		SELECT card_id 
 		FROM `hands` 
